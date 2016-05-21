793d1ab887f70dbf9b22c06288945a89a4769a96
Harry Ragan <harry.ragan@gmail.com>
2016-05-21 17:47:47 +0100

    Initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/russross/blackfriday"
+	_ "github.com/lib/pq"
+)
+
+var (
+	repeat int
+)
+
+func repeatFunc(c *gin.Context) {
+	var buffer bytes.Buffer
+	for i := 0; i < repeat; i++ {
+		buffer.WriteString("Hello from Go!")
+	}
+	c.String(http.StatusOK, buffer.String())
+}
+
+func main() {
+	var err error
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	tStr := os.Getenv("REPEAT")
+	repeat, err = strconv.Atoi(tStr)
+	if err != nil {
+		log.Print("Error converting $REPEAT to an int: %q - Using default", err)
+		repeat = 5
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.GET("/mark", func(c *gin.Context) {
+		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
+	})
+
+	router.GET("/repeat", repeatFunc)
+
+	router.Run(":" + port)
+}
